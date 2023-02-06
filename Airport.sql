33ba227b29e746765c78e445da2bd900a0da48d3
Viktoria Ivanova <malenbkaja94@gmail.com>
2023-02-06 14:32:03 +0200

    Update Airport.sql

--- Airport.sql
+++ Airport.sql
@@ -1,64 +1,80 @@
+--TABLE1 Employees: Employee_id int, F_name varchar (50), L_name varchar (50), Seniority int, Salary int, Nationality varchar (50)
+--TABLE2 Aircrafts: Code int , Manufacture varchar (50), Model int, Type varchar (50), Seatsint varchar (50), Cargo int
+--TABLE3 Flights: Flight_No int, Destination varchar (50), Employee_id int, AC_Code int, Connection varchar (50), Price int
+
+
 1 .מצא את מס' הקוד של המטוסים בחברה שיכולים להוביל מעל 420 נוסעים )כיסאות( וכנה את העמודה בשם AIRCRAFTS BIG
-select Code as "BIG AIRCRAFT"
-   from Aircraft
-   where seat>420;
+
+   SELECT Code as "BIG AIRCRAFT"
+   FROM Aircraft
+   WHERE seat > 420;
 
 2 .מצא את סכום המשכורות שמשולמים לטייסים הקרואטים, לא כולל טייסים שהשכר שלהם נמוך או שווה ל-20000( דולר(
-select sum(salary)
-   from Employees 
-   where Nationality='Croatia'
-   and Salary>20000;
+
+   SELECTsum(salary)
+   FROM Employees 
+   WHERE Nationality='Croatia'
+   AND Salary > 20000;
 
 3 .מצא את שמות המשפחה והשמות הפרטיים של הטייסים שעובדים על הקווים הבאים: לונדון, רומא, איסטנבול
-select E.L_name,E.F_name
-   from Employees as E inner join Flights as F
-   on E.Employee_id=F.Employee_id
-   where F.Destination in('London','Rome','Istanbul');
+  
+   SELECT E.L_name,E.F_name
+   FROM Employees AS E INNER JOIN Flights AS F
+   ON E.Employee_id=F.Employee_id
+   WHERE F.Destination in('London','Rome','Istanbul');
 
 4 .מחק לכל המטוסים מסוג AIRBUS את המטען המותר לטובת ערך ריק בטבלה )ללא מודל: A380)
-update Aircraft
-   set Cargo=null
-   where Model<>'A380';
+   
+   UPDATE Aircraft
+   SET Cargo=null
+   WHERE Model<>'A380';
 
 5 .מצא את ממוצע המשכורות של כלל הטייסים שטסים על מטוסים שמספרם נע בין 345-320( לא כולל(
-select avg(E.Salary)
-    from Employees as E inner join Flights as F
-    on E.Employee_id=F.Employee_id
-    where F.AC_Code between 321 and 344;
+   
+    SELECT AVG(E.Salary)
+    FROM Employees AS E INNER JOIN Flights AS F
+    ON E.Employee_id=F.Employee_id
+    WHERE F.AC_Code BETWEEN 321 AND 344;
 
 6 .מצא מהי המשכורת הגבוהה ביותר של כל הטייסים שמספרם האישי )id_Employee )הוא 145 או 16 או 11 או 57
-select max(salary)
-    from Employees
-    where Employee_id in(145,16,11,57);
+    
+    SELECT MAX(salary)
+    FROM Employees
+    WHERE Employee_id IN(145,16,11,57);
 
 7 .מצא לאילו יעדים טסים הטייסים הוותיקים ביותר בחברה )10 > seniority)
-select distinct(F.Destination)
-   from Employees as E inner join  Flights as F
-   on E.Employee_id=F.Employee_id
-   where E.Seniority>10;
+   
+   SELECT DISTINCT(F.Destination)
+   FROM Employees AS E INNER JOIN  Flights AS F
+   ON E.Employee_id=F.Employee_id
+   WHERE E.Seniority>10;
 
 8 .מצא את מס' הכיסאות שקיימים במטוס בואינג ממודול 767
-select Seats
-    from Aircraft
-    where Manufactory='Boeing' and Model=767;
+
+    SELECT Seats
+    FROM Aircraft
+    WHERE Manufactory='Boeing' AND Model=767;
 
 9 .סדר את טבלת הטיסות לפי יעד בסדר אלפביתי עולה וסידור משני לפי מס' הטיסה יורד )ללא טיסות היוצאות ליעדים: סופיה או קייב(
-select *
-   from Flights
-   where Destination not in('Sofia','kiev')
-   order by Destination,Flight_No desc; 
+
+   SELECT * FROM Flights
+   WHERE Destination NOT IN('Sofia','kiev')
+   ORDER BY Destination,Flight_No DESC; 
 
 10 .מצא את שמותיהם הפרטיים של הטייסים שלא הוגדר להם וותק )SENIORITY)
-select F_name
-     from Employees
-     where Seniority is null;
+
+     SELECT F_name
+     FROM Employees
+     WHERE Seniority IS NULL;
 
 11 .עדכן ברשימת הטיסות מחדש את העיר ת"א לטובת ת"א-יפו
-update Flights
-     set Destination='TLV_J'
-     where Destination='TLV';
+
+     UPDATE Flights
+     SET Destination='TLV_J'
+     WHERE Destination='TLV';
 
 12 .שלוף את כלל המידע של הטייסים ואם הם גם פעילים בטיסות בהווה, ציין את מס' הטיסה שהם אחראים עליה
-select E.*,F.Flight_No
-     from Employees as E left join Flights as F
-     on  Employee_id=F.Employee_id;
+
+     SELECT E.*,F.Flight_No
+     FROM Employees AS E LEFT JOIN Flights AS F
+     ON Employee_id=F.Employee_id;
